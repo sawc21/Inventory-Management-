9eb2ce11729a61b04ab31eb9ce020ad76284cca5
jesdev237 <jreese2507@outlook.com>
2025-12-01 00:14:38 -0600

    New service for file input/output

---
+++ src/main/java/com/ims/io/CsvInventoryStorage.java
@@ -0,0 +1,53 @@
+package com.ims.io;
+
+import com.ims.model.Item;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+interface InventoryFileStorage {
+    List<Item> loadAll(String fileName) throws IOException;
+
+	void saveAll(List<Item> items, String fileName) throws IOException;
+}
+
+//class for loading and saving from/to csv file
+public class CsvInventoryStorage implements InventoryFileStorage {
+	@Override
+	public List<Item> loadAll(String fileName) throws IOException {
+		//create a new empty list object
+        List<Item> items = new ArrayList<>();
+        //try-with-resources to auto-close the file when finished reading
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+        	//skip the header line
+        	reader.readLine();
+        	
+        	String line;
+            while ((line = reader.readLine()) != null) {
+                //separates properties and add to list as new item
+                String[] properties = line.split(",");
+                Item item = new Item(properties[0], properties[1], Integer.parseInt(properties[2]), Double.parseDouble(properties[3]), properties[4]);
+                items.add(item);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return items;
+	}
+	@Override
+	public void saveAll(List<Item> items, String fileName) throws IOException {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+			writer.write("ID, Name, Quantity, Price, Supplier\n");
+			for (Item item1 : items) {
+    			writer.write(item1.getId() + "," + item1.getName() + "," + item1.getQuantity() + "," + item1.getPrice() + "," + item1.getSupplier());
+                writer.newLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+}
